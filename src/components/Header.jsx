e25c0a670ce34f8ca6c0916500c359121984ba0b
Alistair_Piot <alistair.pro15@gmail.com>
2024-04-07 15:46:08 +0200

    Creating Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <h1 className="bg text-colorMain text-2xl font-bold text-center border-b-2">
+                Header
+            </h1>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+function Home() {
+    return (
+        <body className="bg-bgMain min-h-screen">
+            <Header />
+        </body>
+    );
+}
+
+export default Home;
