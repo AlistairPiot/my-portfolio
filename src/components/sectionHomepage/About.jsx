ad763baa68ec063bb4bdd9ea35c2e6771ce529e8
Alistair_Piot <alistair.pro15@gmail.com>
2025-02-07 23:48:12 +0100

    Modif âge

--- src/components/sectionHomepage/About.jsx
+++ src/components/sectionHomepage/About.jsx
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="mb-10 flex size-full flex-col items-center justify-center text-sky-900 lg:mb-14">
             <p className="mt-14 flex w-10/12 items-center justify-center text-center text-2xl md:w-10/12">
-                I'm 21yo, passionate about front-end development and UX/UI
+                I'm 22yo, passionate about front-end development and UX/UI
                 Design
             </p>
             <div className="my-10 w-11/12 border-b-2 border-sky-900 md:w-10/12 lg:w-8/12"></div>
