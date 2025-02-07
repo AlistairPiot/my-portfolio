c8d4edbd177174b7599def76278312f06573f538
Alistair_Piot <alistair.pro15@gmail.com>
2025-02-08 00:25:49 +0100

    petite modif 3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "./", // Essentiel pour GitHub Pages
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+    },
 });
